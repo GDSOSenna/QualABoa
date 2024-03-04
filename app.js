2c62fba6565abdfe93d8a8953c05285edb11c27a
Gustavo Senna Duarte Oliveira <gustavo.duarte@fleetcor.com.br>
2024-03-04 16:20:53 -0300

    Página de criação de eventos :sparkle:

---
+++ app.js
@@ -0,0 +1,29 @@
+function entrar(){
+    
+    window.location.href = 'paginaInicial.html'
+   
+}
+
+function criarAcesso(){
+    
+    window.location.href = 'cadastro.html'
+   
+}
+
+function voltar(){
+    
+    window.location.href = 'index.html'
+   
+}
+
+function inicio(){
+    
+    window.location.href = 'paginaInicial.html'
+   
+}
+
+function criar(){
+    
+    window.location.href = 'criarEvento.html'
+   
+}
