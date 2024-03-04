f006394d1498d23e0d2d227afa0caedcea4a7f86
Gustavo Senna <131923516+GDSOSenna@users.noreply.github.com>
2024-03-03 21:55:57 -0300

    Add files via upload

---
+++ QualABoa/app.js
@@ -0,0 +1,17 @@
+function entrar(){
+    
+    window.location.href = 'mainPage.html'
+   
+}
+
+function criar(){
+    
+    window.location.href = 'cadastro.html'
+   
+}
+
+function voltar(){
+    
+    window.location.href = 'index.html'
+   
+}
